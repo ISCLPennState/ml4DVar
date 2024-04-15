b8c76495395c485b5a993de3f2d4ac20ddad452f
Matthew Poska <mjp5595@polaris-login-02.hsn.cm.polaris.alcf.anl.gov>
2024-04-15 02:13:20 +0000

    make dense ERA5 dataset

---
+++ process-obs/ERA5_dense_obs.py
@@ -0,0 +1,118 @@
+import h5py
+import os
+import sys
+import numpy as np
+import torch
+
+# Create dense ERA5 observation dataset w/ parallelism
+# sys.argv[1] will be the var_idx to use
+
+sys.path.append('/eagle/MDClimSim/mjp5595/ml4dvar/stormer/')
+from stormer_utils_pangu import StormerWrapperPangu
+from varsStormer import varsStormer
+
+sys.path.append('/eagle/MDClimSim/mjp5595/ml4dvar/')
+sys.path.append('/eagle/MDClimSim/mjp5595/ml4dvar/src')
+from obs_cummulative import *
+
+def read_era5(data,vars_stormer):
+    data_np = np.zeros((len(vars_stormer),128,256))
+    for i,var in enumerate(vars_stormer):
+        data_np[i] = data['input/{}'.format(var)][:]
+    return data_np
+
+vars_stormer = varsStormer().vars_stormer
+vars_units = varsStormer().var_units
+
+irga_obs = h5py.File("/eagle/MDClimSim/mjp5595/ml4dvar/obs/igra_141520_stormer_obs_standardized_360_3.hdf5")
+
+era5_dir = '/eagle/MDClimSim/tungnd/data/wb2/1.40625deg_from_full_res_1_step_6hr_h5df/train/'
+era5_2020_dir = '/eagle/MDClimSim/tungnd/data/wb2/1.40625deg_from_full_res_1_step_6hr_h5df/test/'
+
+lats = np.linspace(-90,90,128)
+lons = np.linspace(0,360,256)
+
+means = np.load('/eagle/MDClimSim/tungnd/data/wb2/1.40625deg_from_full_res_1_step_6hr_h5df/normalize_mean.npz')
+stds = np.load('/eagle/MDClimSim/tungnd/data/wb2/1.40625deg_from_full_res_1_step_6hr_h5df/normalize_std.npz')
+
+#era5_obs = h5py.File('/eagle/MDClimSim/mjp5595/ml4dvar/data/era5_obs.h5', 'a')
+#with h5py.File('/eagle/MDClimSim/mjp5595/ml4dvar/obs/era5_dense_obs.h5', 'w') as era5_obs:
+with h5py.File('/eagle/MDClimSim/mjp5595/ml4dvar/obs/era5_dense_obs-{}.h5'.format(int(sys.argv[1])), 'w') as era5_obs:
+    for year in ['2014','2015','2020']:
+        yr_grp = era5_obs.require_group(year)
+        year_hr_idx = 0
+        for month in list(irga_obs['{}'.format(year)].keys()):
+            mth_grp = era5_obs['{}'.format(year)].require_group(str(month))
+            for day in list(irga_obs['{}/{}'.format(year,month)].keys()):
+                day_grp = era5_obs['{}/{}'.format(year,month)].require_group(day)
+                for hour in list(irga_obs['{}/{}/{}'.format(year,month,day)].keys()):
+                    if int(hour) % 6 != 0:
+                        continue
+
+                    print('{}/{}/{}/{} - {:0>4d}'.format(year,month,day,hour,year_hr_idx))
+                    hr_group = era5_obs['{}/{}/{}'.format(year,month,day)].require_group(hour)
+
+                    # load era5
+                    if year == '2014' or year == '2015':
+                        try:
+                            era5_data = torch.from_numpy(read_era5(h5py.File(os.path.join(era5_dir,'{}_{:0>4d}.h5'.format(year,year_hr_idx))),vars_stormer))
+                        except:
+                            continue
+                    else:
+                        try:
+                            era5_data = torch.from_numpy(read_era5(h5py.File(os.path.join(era5_2020_dir,'{}_{:0>4d}.h5'.format(year,year_hr_idx))),vars_stormer))
+                        except:
+                            continue
+                    year_hr_idx += 1
+
+                    for var_idx,var in enumerate(vars_stormer):
+                        if var_idx != int(sys.argv[1]):
+                            continue
+
+                        era5_var_data = []
+                        era5_var_data_H = []
+                        for row in range(127):
+                            for col in range(255):
+                                era5_var_loc_data = era5_data[var_idx,row,col] \
+                                    + era5_data[var_idx,row+1,col] \
+                                    + era5_data[var_idx,row,col+1] \
+                                    + era5_data[var_idx,row+1,col+1]
+                                era5_var_loc_data = era5_var_loc_data / 4.
+                                era5_var_loc_data_lat = (row+1)*(180./128.) - 90.
+                                era5_var_loc_data_lon = (col+1)*(360./256.)
+                                era5_var_data.append([era5_var_loc_data_lat,
+                                                      era5_var_loc_data_lon,
+                                                      era5_var_loc_data])
+
+                                era5_var_loc_data_H_idx = np.zeros(4)
+                                era5_var_loc_data_H_idx[0] = np.ravel_multi_index([row,col],(128,256))
+                                era5_var_loc_data_H_idx[1] = np.ravel_multi_index([row+1,col],(128,256))
+                                era5_var_loc_data_H_idx[2] = np.ravel_multi_index([row,col+1],(128,256))
+                                era5_var_loc_data_H_idx[3] = np.ravel_multi_index([row+1,col+1],(128,256))
+                                era5_var_loc_data_H_obs = np.array([0.25,0.25,0.25,0.25])
+                                era5_var_data_H.extend(np.stack((era5_var_loc_data_H_idx,
+                                                                era5_var_loc_data_H_obs),
+                                                                axis=1)
+                                )
+
+                                #print()
+                                #print('col :',col)
+                                #print('era5_var_data :',era5_var_data)
+                                #print('era5_var_data_H[-4:] :',era5_var_data_H[-4:])
+
+                        era5_var_data = np.array(era5_var_data)
+                        era5_var_data_H = np.array(era5_var_data_H)
+                        #print('era5_var_data.shape :',era5_var_data.shape)
+                        #print('era5_var_data_H.shape :',era5_var_data_H.shape)
+                        era5_var_data[:,2] = (era5_var_data[:,2] - means[var]) / stds[var]
+
+                        #print()
+                        #print('era5_var_data :',era5_var_data)
+                        #print('era5_var_data_H :',era5_var_data_H)
+
+                        era5_obs_dataset = era5_obs['{}/{}/{}/{}'.format(year,month,day,hour)].create_dataset(
+                            '{}'.format(var), data=era5_var_data, dtype = 'f8'
+                        )
+                        era5_obs_dataset_H = era5_obs['{}/{}/{}/{}'.format(year,month,day,hour)].create_dataset(
+                            '{}_H'.format(var), data=era5_var_data_H, dtype = 'f8'
+                        )
---
+++ process-obs/launch_era5_dense_obs.sh
@@ -0,0 +1,58 @@
+#!/bin/bash --login
+#PBS -l select=10:system=polaris
+#PBS -l walltime=4:00:00
+#PBS -q preemptable
+#PBS -A MDClimSim
+#PBS -l filesystems=home:eagle:grand
+#PBS -m bae
+
+. /etc/profile
+
+RUNSPERNODE=7
+
+TSTAMP=$(date "+%Y-%m-%d-%H%M%S")
+echo "Job started at: {$TSTAMP}"
+
+# Figure out training environment
+if [[ -z "${PBS_NODEFILE}" ]]; then
+    RANKS=$HOSTNAME
+    NNODES=1
+else
+    MASTER_RANK=$(head -n 1 $PBS_NODEFILE)
+    RANKS=$(tr '\n' ' ' < $PBS_NODEFILE)
+    NNODES=$(< $PBS_NODEFILE wc -l)
+fi
+
+echo $NNODES
+# Commands to run prior to the Python script for setting up the environment
+PRELOAD="source /etc/profile ; "
+PRELOAD+="ml conda;"
+PRELOAD+="conda activate climaX;"
+PRELOAD+="cd /eagle/MDClimSim/mjp5595/ml4dvar/process-obs/ ;"
+PRELOAD+="export NODES=1; "
+
+# time python process to ensure timely job exit
+TIMER="timeout 718m "
+
+# Launch the pytorch processes on each worker (use ssh for remote nodes)
+RANK=0
+for NODE in $RANKS; do 
+    for ((lNODE=1;lNODE<=$RUNSPERNODE;lNODE++)); do
+        echo $lNODE
+        if [[ "$NODE" == "$HOSTNAME" ]]; then
+            echo "Launching rank $RANK on local node $NODE"
+            # Training script and parameters
+            CMD="python ERA5_dense_obs.py $RANK"
+            FULL_CMD=" $PRELOAD $TIMER $CMD $@ "
+            eval $FULL_CMD &
+        else
+            echo "Launching rank $RANK on remote node $NODE"
+            CMD="python ERA5_dense_obs.py $RANK"
+            FULL_CMD=" $PRELOAD $TIMER $CMD $@ "
+            ssh $NODE "cd $PWD; $FULL_CMD" &
+        fi
+        RANK=$((RANK+1))
+    done
+done
+
+wait
